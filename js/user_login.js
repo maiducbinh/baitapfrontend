ad9a346aefc6295e171289294be26f8af9b177ad
cahoidangngu <nhinhong.1402@gmail.com>
2024-03-15 08:03:56 +0700

    checklogin

--- js/user_login.js
+++ js/user_login.js
@@ -18,9 +18,19 @@ const userList = [
     userImage: "../../img/user_profile.jpg",
     userClass: "E21CQCN03B",
     userUsername: "b21dccn155",
-    userPasword: "12345",
+    userPasword: "Duongtuann@m09",
+  },
+  {
+    userName: "Bình Nam Quân",
+    userImage: "../../img/user_profile.jpg",
+    userClass: "E21CQCN03B",
+    userUsername: "admin",
+    userPasword: "Duongtuann@m09",
   },
 ];
+
+const passwordRegex =
+  /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
 const usernameLoginField = document.querySelector(".username-field");
 const passowrdLoginField = document.querySelector(".password-field");
@@ -28,15 +38,54 @@ const passowrdLoginField = document.querySelector(".password-field");
 const btnLogin = document.getElementById("btn-login");
 
 function checkLogin() {
-  userList.forEach((user) => {
-    if (true) {
-      localStorage.setItem("userName", user.userName);
-      localStorage.setItem("userPassowrd", user.userPasword);
-      localStorage.setItem("userImage", user.userImage);
-      localStorage.setItem("userClass", user.userImage);
-      // Dashboar direct page
-      window.location.href = "page/user/user_home.html";
+  const username = usernameLoginField.value;
+  const password = passowrdLoginField.value;
+  var check = 0;
+  var idx = -1;
+  var message;
+
+  userList.forEach((user, index) => {
+    if (user.userUsername === username) {
+      check = 1;
+      idx = index;
     }
-    return;
   });
+  if (check == 1) {
+    check = 0;
+    if (passwordRegex.test(password)) {
+      check = 1;
+    } else {
+      message =
+        "Mật khẩu phải bao gồm ít nhất một chữ cái in hoa, ký tự đặc biệt, và một số";
+      alert(message);
+    }
+  } else {
+    message = "Tên đăng nhập không tồn tại";
+    alert(message);
+  }
+  if (check == 1) {
+    const user = userList[idx];
+    localStorage.setItem("userName", user.userName);
+    localStorage.setItem("userPassowrd", user.userPasword);
+    localStorage.setItem("userImage", user.userImage);
+    localStorage.setItem("userClass", user.userImage);
+    window.location.href = "page/user/user_home.html";
+  } else {
+    message = "Mật khẩu không đúng";
+    alert(message);
+  }
 }
+
+// function checkLogin() {
+//   userList.forEach((user) => {
+//     if (true) {
+//       localStorage.setItem("userName", user.userName);
+//       localStorage.setItem("userPassowrd", user.userPasword);
+//       localStorage.setItem("userImage", user.userImage);
+//       localStorage.setItem("userClass", user.userImage);
+//       // Dashboar direct page
+//       window.location.href = "page/user/user_home.html";
+//     }
+//     return;
+//   });
+// }
